4e9781de1b9cf0d61cbafb13ec70ccd1f47c36a8
adialkozha <49197733+adialkozha@users.noreply.github.com>
2020-03-02 14:07:34 +0600

    Hide and Show js
    Hide and Show event.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 'use strict'
-
-console.log('ready')
+let modalhide = document.getElementById('modal');
+let item = document.getElementById('item')
+let X = document.getElementById('X')
+function hide(){
+   modalhide.style.display = "none";
+};
+function show(){
+   modalhide.style.display = "block"; 
+};
+X.addEventListener("click",hide);
+item.addEventListener("click",show);
